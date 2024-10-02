ba7629d56f9d1abe40307a0c0f640d583af6c8f9
Thejas <teju270934@gmail.com>
2024-10-02 13:34:22 +0530

    created the get all posts api

--- api/routes/postRoute.js
+++ api/routes/postRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import {verifyToken} from "../utils/verifyUser.js";
-import { create } from "../controllers/postController.js";
+import { verifyToken } from "../utils/verifyUser.js";
+import { create, getposts } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
-
-export default router
+router.get("/getposts", getposts);
+export default router;
