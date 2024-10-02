7660466b5521864067a0e56b33bdae005439cd7e
Thejas <teju270934@gmail.com>
2024-10-02 23:15:55 +0530

    showing users to admin is addded

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,11 @@
 import { Sidebar } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
+import {
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiUser,
+} from "react-icons/hi";
 import { Link, useLocation } from "react-router-dom";
 
 import { signoutSuccess } from "../redux/user/userSlice";
@@ -14,7 +19,7 @@ function DashSidebar() {
 
   const dispatch = useDispatch();
 
-  const {currentUser} = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -65,6 +70,18 @@ function DashSidebar() {
                 as="div"
               >
                 Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=users">
+              <Sidebar.Item
+                active={tab === "posts"}
+                icon={HiOutlineUserGroup}
+                as="div"
+              >
+                Users
               </Sidebar.Item>
             </Link>
           )}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,8 @@ import { useLocation } from "react-router-dom";
 import DashSidebar from "../components/DashSidebar";
 import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
+import DashUsers from "../components/DashUsers";
+
 function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState("");
@@ -25,7 +27,11 @@ function Dashboard() {
         {/* profile section */}
         {tab === "profile" && <DashProfile />}
 
+        {/* posts */}
         {tab === "posts" && <DashPosts />}
+
+        {/* users */}
+        {tab === "users" && <DashUsers />}
       </div>
     </div>
   );
