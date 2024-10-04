eb44b49f8c06869ffc898df8c8b4d67c9e694be8
Thejas <teju270934@gmail.com>
2024-10-04 20:47:57 +0530

    added comments secrtion to side bar

--- api/routes/commentRoute.js
+++ api/routes/commentRoute.js
@@ -5,6 +5,7 @@ import {
   createComment,
   deleteComment,
   editComment,
+  getcomments,
   getPostComments,
   likeComment,
 } from "../controllers/commentController.js";
@@ -16,4 +17,6 @@ router.get("/getPostComments/:postId", getPostComments);
 router.put("/likeComment/:commentId", verifyToken, likeComment);
 router.put("/editComment/:commentId", verifyToken, editComment);
 router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
+router.get("/getcomments", verifyToken, getcomments);
+
 export default router;
